1a2d494e67226eccc26228dc5fc380bf4b6e45f9
Pete Gonzalez <4673363+octogonz@users.noreply.github.com>
2025-06-25 14:01:28 -0700

    - Extract compressString()/decompressString() into separate APIs, rather than magically trying to guess whether the binary data is "text" - The LZMA class avoids unnecessary calls to Int8Array(), but we accept this overhead for the convenience methods - Export the LZMA class

--- index.ts
+++ index.ts
@@ -4,4 +4,5 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-export { compress, decompress } from "./lzma.js";
+export { compress, compressString, decompress, decompressString, LZMA } from "./lzma.js";
+
